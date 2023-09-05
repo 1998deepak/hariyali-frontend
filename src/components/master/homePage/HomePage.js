0505e4296b5d88c417e6d547d2c0613fe472cc59
DESKTOP-SSR7LBK\admin <dighole.mitali@gmail.com>
2023-09-05 13:54:21 +0530

    ui changes

--- src/components/master/homePage/HomePage.js
+++ src/components/master/homePage/HomePage.js
@@ -196,24 +196,24 @@ function HomePage() {
   //   navigate("/Donate");
   // };
   // const navigate = useNavigate();
-  useEffect(() => {
-    window.addEventListener('error', e => {
-      if (e.message === 'ResizeObserver loop limit exceeded') {
-        const resizeObserverErrDiv = document.getElementById(
-          'webpack-dev-server-client-overlay-div'
-        );
-        const resizeObserverErr = document.getElementById(
-          'webpack-dev-server-client-overlay'
-        );
-        if (resizeObserverErr) {
-          resizeObserverErr.setAttribute('style', 'display: none');
-        }
-        if (resizeObserverErrDiv) {
-          resizeObserverErrDiv.setAttribute('style', 'display: none');
-        }
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   window.addEventListener('error', e => {
+  //     if (e.message === 'ResizeObserver loop limit exceeded') {
+  //       const resizeObserverErrDiv = document.getElementById(
+  //         'webpack-dev-server-client-overlay-div'
+  //       );
+  //       const resizeObserverErr = document.getElementById(
+  //         'webpack-dev-server-client-overlay'
+  //       );
+  //       if (resizeObserverErr) {
+  //         resizeObserverErr.setAttribute('style', 'display: none');
+  //       }
+  //       if (resizeObserverErrDiv) {
+  //         resizeObserverErrDiv.setAttribute('style', 'display: none');
+  //       }
+  //     }
+  //   });
+  // }, []);
   return (
     <>
       {/* body */}
