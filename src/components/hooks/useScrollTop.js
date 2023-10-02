35553455f73ebdcb9584cfb2f327451fcc1a4e43
Shivanand Ghule <ghule.shivanand2@mahindra.com>
2023-10-03 00:33:49 +0530

    use scroll hook modified

--- src/components/hooks/useScrollTop.js
+++ src/components/hooks/useScrollTop.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useEffect } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 const useScrollTop = () => {
-    useEffect(() => {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth', // This adds a smooth scroll animation
-        });
-      }, [])
+  // new code by pankaj
+  useEffect(() => {
+    const scrollToTop = () => {
+      scroll.scrollToTop({ duration: 500, smooth: 'easeInOutQuart' });
+    };
+
+    scrollToTop();
+  }, []);
+    // useEffect(() => {
+    //     window.scrollTo({
+    //       top: 0,
+    //       behavior: 'smooth', // This adds a smooth scroll animation
+    //     });
+    //   }, [])
 }
 
 export default useScrollTop
